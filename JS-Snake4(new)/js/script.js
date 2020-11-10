56b235039167883dad8e35e62c1228452b131984
Mattia <mattia.tummolilllo@gmail.com>
2020-11-10 13:37:44 +0100

    Completamento JS 5 (2)

---
+++ JS-Snake4(new)/js/script.js
@@ -0,0 +1,5 @@
+// JS 4 (NEW)
+// Partendo da un array semplice tipo questo, chiediamo all’utente con 2 prompt due numeri tra 0 e la lunghezza dell’array
+// quindi creiamone uno nuovo con solo i valori che hanno la posizione compresa tra i due numeri inseriti dall’utente
+
+var myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
---
+++ JS-Snake5(new)/js/script.js
@@ -0,0 +1,42 @@
+// JS 5 (NEW)
+// dato questo array di obj iniziale, creiamo una copia dell’array di partenza e aggiungiamo ai singoli elementi (quindi ogni oggetto del nuovo array) una nuova proprietà position che contiene una lettera casuale. non dobbiamo modificare l’array iniziale
+
+// iniziale
+var arrayObj = [
+  {name: 'Poppy', type: 'tshirt', color: 'red'},
+  {name: 'Jumping', type: 'occhiali', color: 'blue'},
+  {name: 'CrissCross', type: 'scarpe', color: 'black'},
+  {name: 'Jenny', type: 'borsa', color: 'pink'},
+];
+
+
+//copia
+var arrayObj2 = [
+  {name: 'Poppy', type: 'tshirt', color: 'red'},
+  {name: 'Jumping', type: 'occhiali', color: 'blue'},
+  {name: 'CrissCross', type: 'scarpe', color: 'black'},
+  {name: 'Jenny', type: 'borsa', color: 'pink'},
+];
+
+for (var i = 0; i < arrayObj2.length; i++) {
+   arrayObj2[i].position = randomWord();
+}
+
+console.log(arrayObj2);
+
+
+
+
+
+
+
+
+
+
+// random Word
+
+function randomWord() {
+   var randomPosition = Math.floor(Math.random() * 26);
+   var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+   return alphabet[randomPosition];
+}
